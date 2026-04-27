0043f5a44a5c2cb7dc10fd1b71a40baef88d8cab
Nemo289 <lh2954361@gmail.com>
2026-04-27 04:53:09 +0000

    Install Zustand and create useIngredientStore with slot-based state
    - Add zustand dependency
    - Create src/store/useIngredientStore.ts with slots, baseType, selectedBowl state
    - Add setBaseType, setBowl, clearSelection setters
    - Add placeholder addIngredient and removeIngredient functions

---
+++ src/store/useIngredientStore.ts
@@ -0,0 +1,29 @@
+import { create } from "zustand"
+import type { Bowl, Ingredient } from "../types"
+
+interface IngredientStore {
+  slots: Record<string, Ingredient | null>
+  baseType: number
+  selectedBowl: Bowl | null
+  setBaseType: (id: number) => void
+  setBowl: (bowl: Bowl) => void
+  clearSelection: () => void
+  addIngredient: (item: Ingredient) => void
+  removeIngredient: (id: number) => void
+}
+
+export const useIngredientStore = create<IngredientStore>((set) => ({
+  slots: {},
+  baseType: 1,
+  selectedBowl: null,
+
+  setBaseType: (id) => set({ baseType: id }),
+  setBowl: (bowl) => set({ selectedBowl: bowl }),
+
+  clearSelection: () =>
+    set({ slots: {}, selectedBowl: null, baseType: 1 }),
+
+  addIngredient: () => {},
+
+  removeIngredient: () => {},
+}))
